c6f5f4b91336dafa54215b3689cfaa17941390e7
waizin28 <waizinlinn.08@gmail.com>
2024-10-26 19:28:51 -0500

    added metadata for dynamic and static page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -2,6 +2,15 @@ import { getMeal } from '@/lib/meals';
 import classes from './page.module.css';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
+
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
 
